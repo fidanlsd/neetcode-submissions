585670404df1502f92091de656b1dda05478fe0c
Fidan Asadli <66735231+fidanlsd@users.noreply.github.com>
2026-08-11 16:43:55 +0400

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/is-anagram/submission-0.cs
@@ -0,0 +1,8 @@
+public class Solution {
+    public bool IsAnagram(string s, string t) {
+    
+       if(s.Length !=t.length)
+       return false;
+      Dictionary<char, int> countS = new();
+    }
+}
