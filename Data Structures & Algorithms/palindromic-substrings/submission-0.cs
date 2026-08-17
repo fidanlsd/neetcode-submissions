38288c5f073c03c52cc20fc298860aaba356444f
Fidan Asadli <66735231+fidanlsd@users.noreply.github.com>
2026-08-17 22:27:58 +0400

    Add: palindromic-substrings - submission-0

---
+++ Data Structures & Algorithms/palindromic-substrings/submission-0.cs
@@ -0,0 +1,19 @@
+public class Solution {
+    public int CountSubstrings(string s) {
+        int res = 0, n = s.Length;
+        bool[,] dp = new bool[n, n];
+
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = i; j < n; j++) {
+                if (s[i] == s[j] &&
+                   (j - i <= 2 || dp[i + 1, j - 1])) {
+
+                    dp[i, j] = true;
+                    res++;
+                }
+            }
+        }
+
+        return res;
+    }
+}
