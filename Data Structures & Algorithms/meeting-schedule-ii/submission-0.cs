f37fcd4edc2ab6b5736e60d26d0db60dfca41047
Fidan Asadli <66735231+fidanlsd@users.noreply.github.com>
2026-08-20 00:04:01 +0400

    Add: meeting-schedule-ii - submission-0

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-0.cs
@@ -0,0 +1,37 @@
+/**
+ * Definition of Interval:
+ * public class Interval {
+ *     public int start, end;
+ *     public Interval(int start, int end) {
+ *         this.start = start;
+ /**
+ * Definition of Interval:
+ * public class Interval {
+ *     public int start, end;
+ *     public Interval(int start, int end) {
+ *         this.start = start;
+ *         this.end = end;
+ *     }
+ * }
+ */
+
+public class Solution {
+    public int MinMeetingRooms(List<Interval> intervals) {
+        List<int[]> time = new List<int[]>();
+        foreach (var i in intervals) {
+            time.Add(new int[] { i.start, 1 });
+            time.Add(new int[] { i.end, -1 });
+        }
+
+        time.Sort((a, b) => a[0] == b[0] ?
+            a[1].CompareTo(b[1]) : a[0].CompareTo(b[0]
+        ));
+
+        int res = 0, count = 0;
+        foreach (var t in time) {
+            count += t[1];
+            res = Math.Max(res, count);
+        }
+        return res;
+    }
+}
