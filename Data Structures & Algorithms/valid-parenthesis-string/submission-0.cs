3ed7f3b909f7b32f5c0db39b398b9d083cf24486
Fidan Asadli <66735231+fidanlsd@users.noreply.github.com>
2026-08-19 23:38:40 +0400

    Add: valid-parenthesis-string - submission-0

---
+++ Data Structures & Algorithms/valid-parenthesis-string/submission-0.cs
@@ -0,0 +1,26 @@
+
+public class Solution {
+    public bool CheckValidString(string s) {
+        int leftMin = 0, leftMax = 0;
+
+        foreach (char c in s) {
+            if (c == '(') {
+                leftMin++;
+                leftMax++;
+            } else if (c == ')') {
+                leftMin--;
+                leftMax--;
+            } else {
+                leftMin--;
+                leftMax++;
+            }
+            if (leftMax < 0) {
+                return false;
+            }
+            if (leftMin < 0) {
+                leftMin = 0;
+            }
+        }
+        return leftMin == 0;
+    }
+}
