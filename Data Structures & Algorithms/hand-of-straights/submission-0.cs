f7773b6ad480d1e525a681a5a93d71e1d8029a4b
Fidan Asadli <66735231+fidanlsd@users.noreply.github.com>
2026-08-19 23:20:04 +0400

    Add: hand-of-straights - submission-0

---
+++ Data Structures & Algorithms/hand-of-straights/submission-0.cs
@@ -0,0 +1,30 @@
+public class Solution {
+    public bool IsNStraightHand(int[] hand, int groupSize) {
+        if (hand.Length % groupSize != 0) return false;
+
+        Dictionary<int, int> count = new Dictionary<int, int>();
+        foreach (int num in hand) {
+            if (!count.ContainsKey(num)) count[num] = 0;
+            count[num]++;
+        }
+
+        foreach (int num in hand) {
+            int start = num;
+            while (count.ContainsKey(start - 1) && count[start - 1] > 0) {
+                start--;
+            }
+            while (start <= num) {
+                while (count.ContainsKey(start) && count[start] > 0) {
+                    for (int i = start; i < start + groupSize; i++) {
+                        if (!count.ContainsKey(i) || count[i] == 0) {
+                            return false;
+                        }
+                        count[i]--;
+                    }
+                }
+                start++;
+            }
+        }
+        return true;
+    }
+}
